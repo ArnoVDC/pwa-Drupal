046217ddfd13f73a90fb04e21988d13eef85aa7a
Arno Vande Cappelle <arnovdc@msn.com>
2018-03-16 10:20:28 +0100

    formatting php files for drupal

--- src/Controller/PWAController.php
+++ src/Controller/PWAController.php
@@ -12,40 +12,41 @@ use Drupal\pwa\manifestClass;
  */
 class PWAController extends ControllerBase {
 
-    public function pwa_serviceworker_file_data() {
-        $query_string = \Drupal::state()->get('system.css_js_query_string') ?: 0;
-        $path = drupal_get_path('module', 'pwa');
-        $data = '
+  public function pwa_serviceworker_file_data() {
+    $query_string = \Drupal::state()->get('system.css_js_query_string') ?: 0;
+    $path = drupal_get_path('module', 'pwa');
+    $data = '
         importScripts("/' . $path . '/js/serviceworker.js?' . $query_string . '");';
 
-        return new Response($data, 200, [
-            'Content-Type' => 'application/javascript',
-            'Service-Worker-Allowed' => '/',
-        ]);
-    }
+    return new Response($data, 200, [
+      'Content-Type' => 'application/javascript',
+      'Service-Worker-Allowed' => '/',
+    ]);
+  }
 
-    public function pwa_offline_page() {
-        return [
-            '#type' => 'html_tag',
-            '#tag' => 'h1',
-            '#value' => 'You are offline.',
-            '#attributes' => [
-                'data-drupal-pwa-offline' => true,
-            ],
-        ];
-    }
+  public function pwa_offline_page() {
+    return [
+      '#type' => 'html_tag',
+      '#tag' => 'h1',
+      '#value' => 'You are offline.',
+      '#attributes' => [
+        'data-drupal-pwa-offline' => TRUE,
+      ],
+    ];
+  }
 
-    /**
-     * route generates the manifest file for the browser
-     * @return Response
-     */
-    public function pwa_get_manifest() {
-        $manifestClass = new manifestClass();
-        $content = $manifestClass->get_output();
+  /**
+   * route generates the manifest file for the browser
+   *
+   * @return Response
+   */
+  public function pwa_get_manifest() {
+    $manifestClass = new manifestClass();
+    $content = $manifestClass->get_output();
 
-        return new Response($content, 200, [
-            'Content-Type' => 'application/json'
-        ]);
-    }
- 
+    return new Response($content, 200, [
+      'Content-Type' => 'application/json',
+    ]);
+  }
+
 }
--- src/manifestClass.php
+++ src/manifestClass.php
@@ -4,23 +4,24 @@ namespace Drupal\pwa;
 
 class manifestClass {
 
-    private $manifestUri = '';
+  private $manifestUri = '';
 
-    public function __construct() {
-        $this->manifestUri = '/manifest.json';
-    }
+  public function __construct() {
+    $this->manifestUri = '/manifest.json';
+  }
 
-    /**
-     * returns the manifest json string
-     * @return string
-     */
-    public function get_output() {
-        //get values
-        $values = $this->getCleanValues();
+  /**
+   * returns the manifest json string
+   *
+   * @return string
+   */
+  public function get_output() {
+    //get values
+    $values = $this->getCleanValues();
 
 
-        //create output
-        $output = '
+    //create output
+    $output = '
         {
           "name": "' . $values['site_name'] . '",   
           "short_name": "' . $values['short_name'] . '",
@@ -46,73 +47,87 @@ class manifestClass {
         }';
 
 
-        return $output;
+    return $output;
+  }
+
+  /**
+   * function checks the values in config and add default value if necessary
+   *
+   * @return array
+   */
+  private function getCleanValues() {
+    $output = [];
+    $input = [];
+
+    //change config lang
+    $language_manager = \Drupal::languageManager();
+    $lang = $language_manager->getCurrentLanguage()->getId();
+    $language = $language_manager->getLanguage($lang);
+    $language_manager->setConfigOverrideLanguage($language);
+    $config_get = \Drupal::config('pwa.config');
+
+    $config = \Drupal::service('config.factory')->getEditable('pwa.config');
+
+    $input['site_name'] = $config_get->get('site_name');
+    $input['short_name'] = $config_get->get('short_name');
+    $input['background_color'] = $config_get->get('background_color');
+    $input['theme_color'] = $config_get->get('theme_color');
+    $input['image'] = $config_get->get('image');
+    $input['display'] = $config_get->get('display');
+    $input['default_image'] = $config_get->get('default_image');
+    $input['image_small'] = $config_get->get('image_small');
+
+    if ($input["default_image"] == TRUE) {
+      $input['image'] = theme_get_setting('logo')['url'];
+      $input['image_small'] = $input['image'];
     }
 
-    /**
-     * function checks the values in config and add default value if necessary
-     *
-     * @return array
-     */
-    private function getCleanValues() {
-        $output = array();
-        $input = array();
-
-        //change config lang
-        $language_manager = \Drupal::languageManager();
-        $lang = $language_manager->getCurrentLanguage()->getId();
-        $language = $language_manager->getLanguage($lang);
-        $language_manager->setConfigOverrideLanguage($language);
-        $config_get = \Drupal::config('pwa.config');
-
-        $config = \Drupal::service('config.factory')->getEditable('pwa.config');
-
-        $input['site_name'] = $config_get->get('site_name');
-        $input['short_name'] = $config_get->get('short_name');
-        $input['background_color'] = $config_get->get('background_color');
-        $input['theme_color'] = $config_get->get('theme_color');
-        $input['image'] = $config_get->get('image');
-        $input['display'] = $config_get->get('display');
-        $input['default_image'] = $config_get->get('default_image');
-        $input['image_small'] = $config_get->get('image_small');
-
-        if ($input["default_image"] == TRUE) {
-            $input['image'] = theme_get_setting('logo')['url'];
-            $input['image_small'] = $input['image'];
+    foreach ($input as $key => $value) {
+      if ($value !== '') {
+        $output[$key] = $value;
+      }
+      elseif ($config->get($key) !== '') {
+        $output[$key] = $config->get($key);
+      }
+      else {
+        if ($key === 'background_color' || $key === 'theme_color') {
+          $output[$key] = '#ffffff';
+          $config->set($key, '#ffffff')->save();
         }
-
-        foreach ($input as $key => $value) {
-            if ($value !== '') $output[$key] = $value;
-            elseif ($config->get($key) !== '') $output[$key] = $config->get($key);
-            else if ($key === 'background_color' || $key === 'theme_color') {
-                $output[$key] = '#ffffff';
-                $config->set($key, '#ffffff')->save();
-            } else if ($key === 'image' && $input['dafault_image'] != 1) {
-                //todo make img not found
-                $output[$key] = 'url/to/default/img';
-                $config->set($key, 'url/to/default/img')->save();
-            } else if ($key == 'display') {
-                $output[$key] = 'standalone';
-                $config->set($key, 'standalone')->save();
-            } else {
-                $output[$key] = 'default value for ' . $key . ', go to configuration to change';
-                $config->set($key, 'default value for ' . $key . ', go to configuration to change')->save();
+        else {
+          if ($key === 'image' && $input['dafault_image'] != 1) {
+            //todo make img not found
+            $output[$key] = 'url/to/default/img';
+            $config->set($key, 'url/to/default/img')->save();
+          }
+          else {
+            if ($key == 'display') {
+              $output[$key] = 'standalone';
+              $config->set($key, 'standalone')->save();
             }
+            else {
+              $output[$key] = 'default value for ' . $key . ', go to configuration to change';
+              $config->set($key, 'default value for ' . $key . ', go to configuration to change')
+                ->save();
+            }
+          }
         }
-        //values that's not required
-        $output['description'] = $config_get->get('description');
-
-        return $output;
+      }
     }
+    //values that's not required
+    $output['description'] = $config_get->get('description');
 
-    /**
-     *function deletes the images that is used for the manifest file
-     */
-    public function delete_image() {
-        $config = \Drupal::config('pwa.config');
-        $path = getcwd() . $config->get('image');
-        unlink($path);
-        $path .= 'copy.png';
-        unlink($path);
-    }
+    return $output;
+  }
+
+  /**
+   *function deletes the images that is used for the manifest file
+   */
+  public function delete_image() {
+    $config = \Drupal::config('pwa.config');
+    $path = getcwd() . $config->get('image');
+    unlink($path);
+    $path .= 'copy.png';
+    unlink($path);
+  }
 }
